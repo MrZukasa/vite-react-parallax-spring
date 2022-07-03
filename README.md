dd7e9306fe2eb3b6b314b7b5ed8d254d8d06d6cc
Francesco <fra.boasso@gmail.com>
2022-07-03 16:22:21 +0200

    Aggiunto link documentazione

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 - npm install react-spring
 - npm i @react-spring/parallax
 - npm i @react-spring/web
+
+un po di documentazione [qui](https://openbase.com/js/@react-spring/parallax)
--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import React, { useRef } from 'react'
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 
 export default function App() {
-  
+
   return (
     <Parallax pages={2} style={{ top: '0', left: '0' }}>
       <ParallaxLayer
         offset={0}
         speed={2.5}
-        style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: '#6EB1FF'}}>
         <p>Scroll down</p>
       </ParallaxLayer>
 
